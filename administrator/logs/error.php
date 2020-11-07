d8760990d386ea5b52244c9afd9b391b37c22d22
Jhonatan Nobre <jh_nbs@outlook.com>
2020-11-07 01:38:50 -0400

    instalação portal joomla

---
+++ administrator/logs/error.php
@@ -0,0 +1,15 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2020-11-07 05:14:07 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2020-11-07T05:14:07+00:00	INFO ::1	joomlafailure	Não é permitido senha vazia
+2020-11-07T05:14:32+00:00	INFO ::1	joomlafailure	Nome de usuário e senha incorretos ou você ainda não possui uma conta.
+2020-11-07T05:14:48+00:00	INFO ::1	joomlafailure	Nome de usuário e senha incorretos ou você ainda não possui uma conta.
+2020-11-07T05:14:55+00:00	INFO ::1	joomlafailure	Não é permitido senha vazia.
+2020-11-07T05:14:58+00:00	INFO ::1	joomlafailure	Não é permitido senha vazia.
+2020-11-07T05:15:24+00:00	INFO ::1	joomlafailure	Não é permitido senha vazia.
+2020-11-07T05:15:39+00:00	INFO ::1	joomlafailure	Nome de usuário e senha incorretos ou você ainda não possui uma conta.
+2020-11-07T05:16:57+00:00	INFO ::1	joomlafailure	Nome de usuário e senha incorretos ou você ainda não possui uma conta.
+2020-11-07T05:18:39+00:00	INFO ::1	joomlafailure	Não é permitido senha vazia
